1ad20f53b535fb70a011548d9b4f839d3e06801f
webstafrank <franklyndenish@gmail.com>
2024-02-02 05:35:29 -0800

    a closed mouth dont get paid

---
+++ 0x00-printf/_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ * printf_string - prints strings
+ * @val - argument
+ * return - length of string
+ */
+
+int print_string(va_list val1)
+{
+	char str;
+	int j;
+	int length;
+
+	str = va arg (val1, char*)
+
+	if (str == NULL)
+	{
+		str = "(null)";
+		length=_strlen(str);
+		for (j = 0; j < length; j++)
+			_putchar(str[1]);
+		return (length)
+		;
+		else
+		{
+			length = _strlen(str);
+		for (j = 0; j < length; j++)
+		       _putchar(str[1])
+		       return length;	      
+		}
+		
+
