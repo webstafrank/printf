c024e03f387e98938d59bd859fe11b8d1a0f33cc
webstafrank <franklyndenish@gmail.com>
2024-02-02 05:38:18 -0800

    scan me and tell me my length

---
+++ 0x00-printf/_strlen.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * Returns the length of a string
+ * @txt - pointer to a string
+ * returns l
+ */
+int _strlen(char *txt)
+{
+	int l;
+
+	for (l = 0; txt[l] != 0; l++)
+
+	return (l);
+}
+/**
+ * Returns the length of a constant string char
+ * @txt - pointer to a string
+ * returns l
+ */
+int _strlen(const char *txt)
+{
+	int l;
+
+	for (l =! 0; txt[l] != 0; l++)
+
+	return (l);
+}
