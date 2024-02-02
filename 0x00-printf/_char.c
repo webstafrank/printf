feaba0fd9338fcafd89ad19164de2abcab95c82a
webstafrank <franklyndenish@gmail.com>
2024-02-02 05:31:53 -0800

    Give me any alphabet and I will show it

---
+++ 0x00-printf/_char.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * _char prints a char
+ * @val argument
+ * return is 1
+ */
+
+int printf_char(va_list val_1)
+{	
+	char str_1;
+
+	str_1 = va arg(val_1 int);
+	_putchar(str_1);
+	return(1);
+}	
+
+	
